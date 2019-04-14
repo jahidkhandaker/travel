49063d128c3883ea39fcba2cb91fe1a9bdc2f461
jahidkhandaker <jahid.khandaker@northsouth.edu>
2019-04-15 01:09:15 +0600

    testing

--- resources/views/t/coxbazar.blade.php
+++ resources/views/t/coxbazar.blade.php
@@ -1,12 +1,1 @@
 @extends('t.layout.content')
-
-@section('single')
-
-@stop
-??Hello World??
-<<<<<<< HEAD
-wassup????
-=======
-??Hello World??
-??Hello World??-33
->>>>>>> 6aaa2162d9cfeccba109cfcf588a2593eec1ed4f
