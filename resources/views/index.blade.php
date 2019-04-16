a6c2f83042114619c3b68903a3a9ffd383802ad7
jahidkhandaker <jahid.khandaker@northsouth.edu>
2019-04-16 17:04:24 +0600

    .

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.content')
+
+@section('index_content')
+ 
+  <section class="top_experiences">
+      <div class="container" action='data'>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>Best Places To Travel in Bangladesh</h1>
+            <br><br>
+          </div>
+        </div>        
+        <div class="row">
+          <form action="demo" method="POST">
+            @csrf
+          @foreach ($pal as $u)
+
+            <div class="col-md-4">
+              <div class="common_sight">
+               <a  href="" ><img class="img-responsive" src="data:image/jpeg;base64,{{ base64_encode( $u->pImg ) }}"/></a>
+                  <h5> <input type="submit" name="v" value="{{$u->pName}}"> </h5>
+                  <p> {{ $u->pDistrict }} </p>
+              </div>
+            </div>
+
+          @endforeach
+          </form>
+        </div>
+      </div>
+  </section> 
+
+
+@stop
---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,423 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bangladesh Tour</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Buenard:700' rel='stylesheet' type='text/css'>
+    
+    
+   
+    <link rel="stylesheet" href="css/bg_css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+
+</head>
+<body>
+ 
+    <section class="navbar_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="logo">
+                        <img src="images/logo_lp.png" class="img-responsive" alt="logo">
+                    </div>
+                </div>
+                
+                 <div class="col-md-8 col-md-offset-2 custom_nav">
+  
+
+                     <nav class="navbar navbar-inverse">
+      
+       
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                        </div>
+                    
+                        <div class="collapse navbar-collapse" id="navbar-collapse-3">
+                          <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Destination</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li>
+                              <a class="btn btn-default btn-outline btn-circle btn btn-primary btn-lg" href="loginpage">Sign In</a>
+                            </li>
+                          </ul>
+                          <div class="collapse nav navbar-nav nav-collapse" id="nav-collapse3">
+                            <form class="navbar-form" role="search">
+                              <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search" />
+                              </div>
+                              <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                            </form>
+                          </div>
+                        </div>
+                    </nav> 
+                </div>
+            </div>
+        </div>       
+    </section>
+
+  
+
+  <section style="background-color: black;">
+    <div class="container">
+      <div class="row text-center" style="color: white;">
+       <h4></h4>
+      </div>
+    </div>
+  </section>
+
+
+     <header>
+
+            <div id="firstPage" class="ha-bg-parallax main_banner " data-type="background" data-speed="10">
+                <div class="ha-parallax-body">
+                    <div id="carousel-example-generic" class="carousel slide vertical text-right" data-ride="carousel"> 
+                     
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                   
+
+                        <div class="container">
+                            <div class="carousel-inner " role="listbox">
+                                <div class="item active">
+                                    <h3 white data-wow-delay="0.1s">Tour To</h3>
+                                    <h1 style="padding-bottom: 12px;" white data-wow-delay="0.2s">Bangladesh</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </header>
+
+    <section class="about_bd">
+        <div class="container text-center welcome">
+              <h2>Welcome to Bangladesh</h2>
+              <p >Bangladesh is south Asia's greenest jewel – a country braided with rivers, <br>with a rich culture waiting to be explored by pioneering travellers.</p>
+              
+              <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#bd_info">Know More</button>
+              <div id="bd_info" class="collapse text-left" >
+
+                  <b>Official Name:</b> <span>The People’s Republic of Bangladesh</span> <br>
+                  <b>State Religion:</b> <span>Islam but other main religions namely Hinduism, Buddhism, Christianity are practiced in peace and harmony.</span><br><br>
+                  <b>State Language:</b> <span>Bangla</span><br><br> 
+
+                  <b>National Emblem:</b><span> The national flower “Shapla” (nymphea-nouchali) resting on water, having on each side an ear of paddy and being surmounted by three connected leaves of jute with two stars on each side of the leaves.</span><br><br>
+
+                  <b>Capital:</b> <span>Dhaka</span><br><br>
+
+                  <b>Nationality:</b> <span>Bangladeshi</span><br><br>
+
+                  <b>Name of Currency:</b> <span>Taka (TK)</span><br><br>
+                  <b>Denominations:</b><br><br>
+                  <b>Notes:</b><span>Taka 1000, 500, 100, 50, 20, 10, 5, 2 & 1</span><br><br>
+                  <b>Coins: </b><span>Taka 5, 2, 1</span><br><br>
+
+                  <b>Geographical Location:</b> <span>Between 20°34′ and 26°38′ north latitude and between 88°01′ and 92°41′ east longitude.Boundary: <br> North: India <br> West: India <br>South: Bay of Bengal <br>East: India and MyanmarArea: 56977 sq. miles or 147570 sq. km.Territorial Water: 12 nautical mile(Political) and 200 nautical miles(Economic). </span><br><br>
+
+                  <b>Geography :</b> <span>Bangladesh is a “Land of Stories” and “Land of Rivers”. Everything of this land tells a story; a story of Bangladeshi people and their heart. Not only the livelihood of Bangladeshi people is deeply rooted in this land, but also their life is deeply incorporated in this lands.</span><br><br>
+
+                 <b> Climate:</b>  <span>Our climate, frankly, complements our lands. It completes the stories of our land. Bangladesh is not only a land of stories, but also a land of six wonderful seasons. People who love the essence of nature are going to fall in love with this wonderland. Seasons that will definitely excite you with their diverse characteristics.</span><br>
+              </div>
+          </div>
+    </section>
+
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <video width="100%" controls autoplay muted>
+                            <source src="images/Bangladesh travel.mp4" type="video/mp4">
+                                    Your browser does not support HTML5 video.
+                    </video>
+                </div>
+            </div>
+        </div>
+    </section>
+ 
+    @yield('index_content')
+    @yield('spot_content')
+ 
+ 
+
+
+  <section class="google_location" style="margin-top: 20px; margin-bottom: 30px;">
+    <div class="container">
+      <div class="row text-center">
+        <h1>Google Map</h1>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center">
+          <div ng-app="locationpickerApp" ng-controller="locationpickerController" id="">
+            <div class="form-horizontal" style="width:100%;">
+              <div class="form-group">
+                <label class="col-md-2 control-label">Location:</label>
+
+                <div class="col-md-10">
+                  <input type="text" class="form-control" id="us3-address"/>
+                </div>
+              </div>
+
+
+              <!--Map by element. Also it can be attribute-->
+              <locationpicker  options="locationpickerOptions" style="width: 100%;"></locationpicker>
+
+              <div class="clearfix">&nbsp;</div>
+              <div class="m-t-small">
+                <label class="p-r-small col-sm-1 control-label">Lat.:</label>
+
+                <div class="col-md-3">
+                  <input type="text" class="form-control" style="" id="us3-lat"/>
+                </div>
+                <label class="p-r-small col-sm-2 control-label">Long.:</label>
+
+                <div class="col-md-3">
+                  <input type="text" class="form-control" style="" id="us3-lon"/>
+                </div>
+              </div>
+              <div class="clearfix"></div>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    
+
+    <footer class="footer1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-md-3">
+                        <ul class="list-unstyled clear-margins">
+                            <li class="widget-container widget_nav_menu">
+                                <h1 class="title-widget ">Useful links</h1>
+                                <ul>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> About Us</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> Contact Us</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> Adventure Stories</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> For Help</a></li>
+                                
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-md-3">
+
+                        <ul class="list-unstyled clear-margins">
+
+                            <li class="widget-container widget_nav_menu">
+
+                                <h1 class="title-widget">Useful links</h1>
+
+                                <ul>
+                                    <li><a  href="https://www.facebook.com/groups/mail.tob/"><i class="fa fa-angle-double-right"></i>  TOB</a></li>
+                                    <li><a  href="https://www.facebook.com/groups/tob.help/"><i class="fa fa-angle-double-right"></i>  TOB Helpline</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Travel agency</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Visa Information</a></li>
+                                    
+                                </ul>
+
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-md-3">
+
+                        <ul class="list-unstyled clear-margins">
+
+                            <li class="widget-container widget_nav_menu">
+
+                                <h1 class="title-widget">Useful links</h1>
+
+                                <ul>
+
+
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> Privecy </a></li>
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> Term and Policy</a></li>
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> Feedback</a></li>
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> Report</a></li>
+
+                                </ul>
+
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-md-3">
+                        <ul class="list-unstyled clear-margins">
+                            <li class="widget-container widget_recent_news">
+
+                                <h1 class="title-widget">Contact Detail </h1>
+
+                                <div class="footerp"> 
+
+                                    <h2 class="title-median">WE4 Pvt. Ltd.</h2>
+                                    <p><b>Email id:</b> <a href="#">jahid.khandaker@northsouth.edu</a></p>
+                                    <p><b>Helpline Numbers </b>
+
+                                        <b style="color:#ffc106;">(8AM to 10PM):</b>  +8801676123950, +8801939303525  </p>
+
+                                        <p><b>Corp Office / Postal Address</b></p>
+                                    </div>
+
+                                    <div class="social-icons">
+
+                                        <ul class="nomargin">
+
+                                            <a href="https://www.facebook.com"><i class="fa fa-facebook-square fa-3x social-fb" id="social"></i></a>
+                                            <a href="https://twitter.com"><i class="fa fa-twitter-square fa-3x social-tw" id="social"></i></a>
+                                            <a href="https://plus.google.com"><i class="fa fa-google-plus-square fa-3x social-gp" id="social"></i></a>
+                                            <a href="#"><i class="fa fa-envelope-square fa-3x social-em" id="social"></i></a>
+
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+    </footer>
+
+    <section>
+                <div class="footer-bottom">
+
+                    <div class="container">
+
+                        <div class="row">
+
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-md-6">
+
+                                <div class="copyright">
+
+                                    © 2018, NSU, All rights reserved
+
+                                </div>
+
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-md-6">
+
+                                <div class="design">
+
+                                   <a href="#">WE4 </a> |  <a target="_blank" href="http://www.northsouth.edu">Web Design & Development by we4</a>
+
+                               </div>
+
+                           </div>
+
+                       </div>
+
+                   </div>
+               </div>
+
+    </section> 
+
+    
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <script src='https://maps.google.com/maps/api/js?sensor=false&libraries=places'></script>
+    <!-- <script src="js/locationpicker.jquery.min.js"></script> -->
+    <script src="js/locationpicker.jquery.js"></script>
+    <!-- <script src="js/angularLocationpicker.jquery.min.js"></script> -->
+    <script src="js/angularLocationpicker.jquery.js"></script>
+
+           <script src="js/bootstrap.min.js"></script>
+           <script src="js/script.js"></script>
+
+
+          <script>
+             function refresh() {
+             location.reload();
+            }
+          </script>
+
+            <script>
+              function myFunction() {
+                alert("Please Sign in To Explore!");
+              }
+            </script>
+
+          <script>
+            $(document).ready(function(){
+                var header = $('.main_banner ');
+
+                var backgrounds = new Array(
+
+                   
+                     ' url(images/Nilgiri.jpg)' ,
+                    'url(images/group_env203.jpg)' ,
+                    'url(images/St_Martin.jpg)'     
+
+
+                    );
+
+                var current = 0;
+
+                function nextBackground() {
+                    current++;
+                    current = current % backgrounds.length;
+                    header.css('background-image', backgrounds[current]);
+                }
+                setInterval(nextBackground, 4000);
+
+                header.css('background-image', backgrounds[0]);
+            });
+          </script>
+
+        <script>
+          angular.module('locationpickerApp', ['angular-jquery-locationpicker'])
+          .controller('locationpickerController', [
+            '$scope',
+            function ($scope) {
+              $scope.locationpickerOptions = {
+                location: {
+                  latitude: 23.815103,
+                  longitude: 90.42553799999996
+                },
+                inputBinding: {
+                  latitudeInput: $('#us3-lat'),
+                  longitudeInput: $('#us3-lon'),
+                  radiusInput: $('#us3-radius'),
+                  locationNameInput: $('#us3-address')
+                },
+                radius: 400,
+                enableAutocomplete: true
+              };
+            }
+            ]);
+        </script>
+
+
+    </body>
+    </html>
