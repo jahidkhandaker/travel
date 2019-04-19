11c9e09eb89c871a0f133a07053ca8a53444fa54
Mugdho <munem.mugdho@northsouth.edu>
2019-04-19 13:37:51 +0600

    new changes

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -20,12 +20,12 @@
 </head>
 <body>
  
-    <section class="navbar_section navbar_custom">
+   <!--  <section class="navbar_section navbar_custom">
         <div class="container">
             <div class="row">
                 <div class="col-md-2">
                     <div class="logo">
-                        <!-- <img src="images/logo_lp.png" class="img-responsive" alt="logo"> -->
+                        
                         <h1> HAPPY TRAVEL </h1>
                     </div>
                 </div>
@@ -70,12 +70,73 @@
             </div>
         </div>       
     </section>
-
+ -->
   
+  <body>
+  
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="logo">
+            <h1>HAPPY TRAVEL</h1>
+          </div>
+        </div>
+        
+        
+
+        <div class="col-md-8">
+          
+          <nav class="navbar navbar-default custom_navbar">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand visible-xs-block" href="#">Minimo</a>
+
+
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav pull-right">
+                  <li><a href="/">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Destination</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li>
+                              <a class="btn btn-default btn-outline btn-circle btn btn-primary btn-lg" href="loginpage">Sign In</a>
+                            </li>
+                </ul>
+                  <div class="collapse nav navbar-nav nav-collapse" id="nav-collapse3">
+                            <form class="navbar-form" role="search">
+                              <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search" />
+                              </div>
+                              <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                            </form>
+                          </div>
+
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
+          
+        </div>
+        
+        
+      </div>
+    </div>
+  </header>
 
   <section style="background-color: inherit;">
     <div class="container">
-      <div class="row text-center" style="color: white;">
+      <div class="row text-center" style="color: #000;">
        <h4></h4>
       </div>
     </div>
